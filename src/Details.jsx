f3ecb61c96d649ce75547d3615d5c8bc24699ca6
khantzinwin2 <khantzinwin11111@gmail.com>
2023-06-29 14:58:07 +0630

    Hello

--- src/Details.jsx
+++ src/Details.jsx
@@ -1,7 +1,7 @@
 import axios from 'axios';
 import React, { useEffect, useState } from 'react'
 import { useParams } from 'react-router-dom'
-import {AiFillYoutube} from "react-icons/ai"
+import {AiFillYoutube } from "react-icons/ai"
 import Loading from './Loader/Loading';
 
 
